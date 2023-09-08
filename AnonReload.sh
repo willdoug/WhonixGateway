2b90cbcc13a8a42fcc3e95668ee57934ee85ab2b
William Castro <williamweb2.0@gmail.com>
2023-09-08 14:14:19 -0300

    Create AnonReload.sh

---
+++ AnonReload.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#this is simple script to reload ip and connectio tor while yout scan target over waf.
+#it try bypass block ip...remember of set in you tool to retry request command if first time fail execution...
+
+#how to use:
+#just run this file in you whonix gateway cli, and he go run forever in loop, while yout work...
+
+#chmod +x AnonReload.sh
+#./AnonReload.sh
+while true
+do
+        echo "ip atual "
+        curl ident.me
+        echo " "
+        sleep 60 
+        /usr/bin/restart-tor-gui
+        echo "ip atual "
+        curl ident.me
+        echo " "
+        sleep 60 
+        sudo service networking restart
+        echo "ip atual "
+        curl ident.me
+        echo " "
+        sleep 60 
+        sudo service tor restart
+        anon-verify
+done
