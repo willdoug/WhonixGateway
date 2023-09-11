551c50922389318141be6eb723059cba533c4481
William Castro <williamweb2.0@gmail.com>
2023-09-11 09:09:09 -0300

    Update AnonReload.sh
    add clock verify's

--- AnonReload.sh
+++ AnonReload.sh
@@ -9,14 +9,15 @@
 #./AnonReload.sh
 while true
 do
-        echo "ip atual "
+   echo "ip atual "
         curl ident.me
         echo " "
-        sleep 60 
+        sleep 300 
         /usr/bin/restart-tor-gui
         echo "ip atual "
         curl ident.me
         echo " "
+        systemcheck --verbose --cli
         sleep 60 
         sudo service networking restart
         echo "ip atual "
@@ -25,4 +26,19 @@ do
         sleep 60 
         sudo service tor restart
         anon-verify
+        echo "ip atual "
+        curl ident.me
+        echo " "
+        sleep 60 
+        anon-info
+        sudo service sdwdate restart
+        echo "ip atual "
+        curl ident.me
+        echo " "
+        systemcheck --verbose --cli 
+        date -u
+        sudo date --utc && sudo hwclock --utc
+        sudo hwclock --utc --systohc
+done
+
 done
